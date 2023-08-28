1a922e3a8066a802d0ad4ffd7d4442dc99937f32
s-c-meyer <meyer.samuelc@gmail.com>
2023-08-28 15:16:22 -0500

    Update main-view.jsx

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import { MovieCard } from "../movie-card/movie-card";
 import { MovieView } from "../movie-view/movie-view";
-
+ 
 export const MainView = () => {
   const [movies, setMovies] = useState([
     {
