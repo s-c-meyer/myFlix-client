7aa971ccedec6c3c43b16c1961a952badd41e5f4
s-c-meyer <meyer.samuelc@gmail.com>
2023-08-29 11:37:03 -0500

    Connected App with Heroku
    Connected myFlix App with Heroku, and added PropTypes to ensure data consistency

--- src/components/movie-card/movie-card.jsx
+++ src/components/movie-card/movie-card.jsx
@@ -1,3 +1,5 @@
+import PropTypes from "prop-types";
+
 export const MovieCard = ({ movie, onMovieClick }) => {
   return (
     <div 
@@ -8,4 +10,17 @@ export const MovieCard = ({ movie, onMovieClick }) => {
       {movie.title}
     </div>
   );
+};
+
+//Here is where all of the MovieCard prop constraints are defined
+MovieCard.propTypes = {
+  movie: PropTypes.shape({
+    title: PropTypes.string.isRequired,
+    image: PropTypes.string.isRequired,
+    description: PropTypes.string.isRequired,
+    director: PropTypes.string.isRequired,
+    genre: PropTypes.string.isRequired,
+    runtime: PropTypes.string.isRequired
+  }).isRequired,
+  onMovieClick: PropTypes.func.isRequired
 };
