5e123a124506ff30fd43a8bd771905c782109f98
s-c-meyer <meyer.samuelc@gmail.com>
2023-08-31 08:58:54 -0500

    Added login and signup views
    Added the two new views for Exercise 3.5

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -1,43 +1,63 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { MovieCard } from "../movie-card/movie-card";
 import { MovieView } from "../movie-view/movie-view";
- 
-export const MainView = () => {
-  const [movies, setMovies] = useState([
-    {
-      id: 1,
-      title: "The Dark Knight",
-      description: "The plot follows the vigilante Batman, police lieutenant James Gordon, and district attorney Harvey Dent, who form an alliance to dismantle organized crime in Gotham City. Their efforts are derailed by the Joker, an anarchistic mastermind who seeks to test how far Batman will go to save the city from chaos.",
-      image: "https://m.media-amazon.com/images/I/91KkWf50SoL._AC_UF894,1000_QL80_.jpg",
-      director: "Christopher Nolan",
-      genre: "Action",
-      runtime: "152"
-    },
-    {
-      id: 2,
-      title: "Pulp Fiction",
-      description: "The lives of two mob hitmen, a boxer, a gangster and his wife, and a pair of diner bandits intertwine in four tales of violence and redemption.",
-      image: "https://m.media-amazon.com/images/M/MV5BNGNhMDIzZTUtNTBlZi00MTRlLWFjM2ItYzViMjE3YzI5MjljXkEyXkFqcGdeQXVyNzkwMjQ5NzM@._V1_.jpg",
-      director: "Quentin Tarantino",
-      genre: "Crime",
-      runtime: "154"
-    },
-    {
-      id: 3,
-      title: "Elf",
-      description: "Raised as an oversized elf, Buddy travels from the North Pole to New York City to meet his biological father, Walter Hobbs, who doesn't know he exists and is in desperate need of some Christmas spirit.",
-      image: "https://m.media-amazon.com/images/M/MV5BMzUxNzkzMzQtYjIxZC00NzU0LThkYTQtZjNhNTljMTA1MDA1L2ltYWdlL2ltYWdlXkEyXkFqcGdeQXVyMTMxODk2OTU@._V1_.jpg",
-      director: "Jon Favreau",
-      genre: "Comedy",
-      runtime: "97"
-    }
-  ]);
+import { LoginView } from "../login-view/login-view";
+import { SignupView } from "../signup-view/signup-view";
 
-  /* by setting the initial state of this variable to null, 
+export const MainView = () => {
+  const storedUser = JSON.parse(localStorage.getItem("user"));
+  const storedToken = localStorage.getItem("token");
+  const [user, setUser] = useState(storedUser? storedUser: null);
+  const [token, setToken] = useState(storedToken? storedToken: null);
+  const [movies, setMovies] = useState([]); //this hook has to be grouped with these ones above it, it throws an error when it is further down in the code
+
+   /* by setting the initial state of this variable to null, 
   this tells the app that no movie cards are currently clicked. 
   Then when you click on one, you have to update the MainView to
   reflect that one of the movies has been clicked */
   const [selectedMovie, setSelectedMovie] = useState(null);
+  
+  useEffect(() => {
+    if (!token) {
+      return;
+    }
+
+    fetch("https://moviesappmyflix-02f853986708.herokuapp.com/movies", {
+      headers: { Authorization: `Bearer ${token}` }
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        const moviesFromApi = data.map((doc) => {
+          return {
+            id: doc._id,
+            title: doc.Title,
+            image: doc.ImagePath,
+            description: doc.Description,
+            director: doc.Director.Name,
+            genre: doc.Genre.Name,
+            runtime: doc.RuntimeMins + ' minutes'
+          };
+        });
+
+        setMovies(moviesFromApi);
+        });
+  }, [token]); //token must be added here, known as the dependency array. It ensures fetch is called every time token changes
+
+  //if there is no one logged in, display the login screen / signup screen
+  if (!user) {
+    return (
+      <>
+        <LoginView
+          onLoggedIn={(user, token) => {
+            setUser(user);
+            setToken(token);
+          }}
+        />
+        OR
+        <SignupView />
+      </>
+    );
+  }
 
   if (selectedMovie) {
     return (
@@ -49,17 +69,20 @@ export const MainView = () => {
     return <div>The list is empty!</div>;
   } else {
     return (
-      <div>
-        {movies.map((movie) => (
-          <MovieCard
-            key={movie.id}
-            movie={movie} //movie is a prop being created
-            onMovieClick={(newSelectedMovie) => {
-              setSelectedMovie(newSelectedMovie);
-            }}
-          />
-        ))}
-      </div>
+      <>
+        <div>
+          {movies.map((movie) => (
+            <MovieCard
+              key={movie.id}
+              movie={movie} //movie is a prop being created
+              onMovieClick={(newSelectedMovie) => {
+                setSelectedMovie(newSelectedMovie);
+              }}
+            />
+          ))} 
+        </div>
+        <button onClick={() => { setUser(null); setToken(null); localStorage.clear(); }}>Logout</button> 
+      </>
     );
   }
 };
